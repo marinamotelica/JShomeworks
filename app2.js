195b523759e71e4bb8e538cd652bc95c3e993f99
Marina Motelica <marina.motelica@gmail.com>
2022-12-14 16:01:24 +0300

    temeleLajs added

---
+++ app2.js
@@ -0,0 +1,26 @@
+
+
+console.log("Hello Marina")
+alert("Hi")
+
+
+let Ctemp = 10;
+let CtemptoFahr = Ctemp * (9/5)+32
+console.log(Ctemp+"grade celsius = "+ CtemptoFahr+"grade farenheit")
+
+function CtoF(num){
+    return num*(9/5)+32
+}
+CtoF (10)
+
+let Ftemp = 10;
+let FtemptoC = (Ftemp-32) * 5/9
+console.log(Ftemp+"grade farenheit = "+ FtemptoC+ "grade celsius")
+
+
+ 
+
+
+
+
+
