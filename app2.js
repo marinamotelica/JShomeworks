10168f27b7719e4153ed02ae3f234598299a74d2
Marina Motelica <marina.motelica@gmail.com>
2022-12-14 16:42:51 +0300

    the 3rd homework added

--- app2.js
+++ app2.js
@@ -17,6 +17,30 @@ let Ftemp = 10;
 let FtemptoC = (Ftemp-32) * 5/9
 console.log(Ftemp+"grade farenheit = "+ FtemptoC+ "grade celsius")
 
+let speed = 17500
+let mars = 225000000
+let moon = 384400
+let miles = 0.621 
+console.log(typeof moon);
+console.log(typeof miles);
+console.log(typeof mars);
+console.log(typeof speed);
+let distanseToMars = mars * miles
+console.log(distanseToMars+ "kilometers");
+let hoursToMars = distanseToMars / speed
+console.log(hoursToMars+"to rich the Mars");
+let day = 24
+let daysToMars = hoursToMars / day
+console.log(daysToMars+"days it will take to the Mars");
+
+
+
+
+
+
+
+
+
 
  
 
